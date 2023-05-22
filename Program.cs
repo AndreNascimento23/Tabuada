76148a953b1533f8fd99c5b00f9b159d7ddae93a
André Nascimento <andre4.20nascimento@outlook.com>
2023-05-22 18:38:44 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+int tabuada, numerador = 0;
+
+Console.Write("Digite o valor da Tabúada..: ");
+tabuada = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+while (numerador <= 10)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{tabuada} * {numerador}= {tabuada * numerador} ");
+    numerador++;
+    Console.ResetColor();
+}
